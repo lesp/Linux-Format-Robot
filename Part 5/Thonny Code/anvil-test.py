7c851e2c9fa32146c902fb3116ab0ac36c7c4065
Les <lespounder@gmail.com>
2023-02-19 14:13:42 +0000

    Part 5, Anvil code
    Code to create a web interface for use with our robot.

---
+++ Part 5/Thonny Code/anvil-test.py
@@ -0,0 +1,50 @@
+import robot
+import anvil.server
+from signal import pause
+import time
+anvil.server.connect("server_GZS5U6CYOSNNSDAZMOEW7FRZ-CQ25HSIID3WXLZMU")
+
+@anvil.server.callable
+def stop():
+    robot.stop()
+    print("Done")
+
+@anvil.server.callable
+def forward():
+    robot.forward(0.5)
+    print("Moving forward")
+
+@anvil.server.callable
+def backward():
+    robot.backward(0.5)
+    print("Moving backward")
+
+@anvil.server.callable
+def left():
+    robot.spin_left(0.5)
+    time.sleep(0.5)
+    robot.stop()
+    print("Spin Left")
+
+@anvil.server.callable
+def right():
+    robot.spin_right(0.5)
+    time.sleep(0.5)
+    robot.stop()
+    print("Spin Right")
+
+@anvil.server.callable
+def dance():
+    for i in range(3):
+        print("Forward")
+        robot.forward(0.5)
+        time.sleep(2)
+        print("Spin Right")
+        robot.spin_right(0.5)
+        time.sleep(1)
+        robot.stop()
+        time.sleep(0.5)
+
+
+
+pause()
